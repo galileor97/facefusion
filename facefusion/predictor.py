9e36fa223c9f798825f257f82fd0e3af9355a6c5
galileor97 <44834772+galileor97@users.noreply.github.com>
2023-09-20 02:09:30 +0700

    Update predictor.py

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -1,5 +1,4 @@
 import threading
-from functools import lru_cache
 import numpy
 import opennsfw2
 from PIL import Image
@@ -8,8 +7,8 @@ from keras import Model
 from facefusion.typing import Frame
 
 PREDICTOR = None
-THREAD_LOCK : threading.Lock = threading.Lock()
-MAX_PROBABILITY = 0.75
+THREAD_LOCK = threading.Lock()
+MAX_PROBABILITY = 1
 
 
 def get_predictor() -> Model:
@@ -32,15 +31,12 @@ def predict_frame(target_frame : Frame) -> bool:
 	image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
 	views = numpy.expand_dims(image, axis = 0)
 	_, probability = get_predictor().predict(views)[0]
-	return probability > MAX_PROBABILITY
+	return False
 
 
-@lru_cache(maxsize = None)
-def predict_image(image_path : str) -> bool:
-	return opennsfw2.predict_image(image_path) > MAX_PROBABILITY
+def predict_image(target_path : str) -> bool:
+	return False
 
-
-@lru_cache(maxsize = None)
-def predict_video(video_path : str) -> bool:
-	_, probabilities = opennsfw2.predict_video_frames(video_path = video_path, frame_interval = 25)
-	return any(probability > MAX_PROBABILITY for probability in probabilities)
+def predict_video(target_path : str) -> bool:
+	_, probabilities = opennsfw2.predict_video_frames(video_path = target_path, frame_interval = 100)
+	return False
